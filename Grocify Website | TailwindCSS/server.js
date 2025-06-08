13fe56de607fb187ce47223a4a2356e67e928dac
AbdusSalam777 <abdusalam0381@gmail.com>
2025-06-08 19:36:07 +0500

    Update server.js

--- Grocify Website | TailwindCSS/server.js
+++ Grocify Website | TailwindCSS/server.js
@@ -1,11 +1,33 @@
+const dotenv=require("dotenv");
+dotenv.config();
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const mongoose=require("mongoose");
 
 const app=express();
 const PORT=3000;
 
-app.use(express.static( "public"));
+app.use(express.static("public"));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("MongoDB connected successfully 👍")
+}).catch((error)=>{
+    console.log(error);
+    
+});
+
+const UserSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:Number,
+    feedback:String
+})
+
+const Usermodel= mongoose.model("Detail",UserSchema);
+
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"index.html"));
@@ -26,4 +48,15 @@ app.get("/contact",(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}` );
     
+})
+
+app.post("/send-to-mongo",(req,res)=>{
+       const data=req.body;
+       Usermodel.create(data).then(()=>{
+        console.log("Data sent to MongoDB 👍");
+        
+       }).catch((error)=>{
+        console.log(error);
+        
+       })
 })
