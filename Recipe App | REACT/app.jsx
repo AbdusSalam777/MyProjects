93fc534071f269788497f1627137b4b37044ffe9
AbdusSalam777 <abdusalam0381@gmail.com>
2025-06-16 00:50:10 +0500

    Update app.jsx

--- Recipe App | REACT/app.jsx
+++ Recipe App | REACT/app.jsx
@@ -1,18 +1,22 @@
 import Nav from "./nav.jsx";
 import Card from "./card.jsx";
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Recipe from "./recipe.jsx";
 
 function App() {
-
-  const [meals,setmeals]=useState([]); 
+  const [meals, setmeals] = useState([]);
 
   return (
-    <>
-       <Nav setmeals={setmeals}/>
-       <Card meals={meals}></Card>
-    </>
+    <BrowserRouter>
+      <Nav setmeals={setmeals} />
 
-  )
+      <Routes>
+        <Route path="/" element={<Card meals={meals} />}></Route>
+        <Route path="/recipe/:id" element={<Recipe/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
